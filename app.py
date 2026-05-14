d844d41fda12b5b62cdee7a2f3b321b0fe603f0a
Flo466 <bruel.florent@gmail.com>
2026-05-14 18:02:18 +0200

    Create first test. User input, GET/POST methods

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=["GET", "POST"])
+def home():
+    result = ''
+    if request.method == 'POST':
+        data = request.form.get('calculation')
+        try:
+            result = eval(data)
+        except:
+            result = 'Error'
+    
+
+    return render_template('index.html', display_result = result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
